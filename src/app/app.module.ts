eaa3e8700fcbc3e34549b7d7f553b0d8084d90f5
Rypeur <duval.quentin01@gmail.com>
2019-03-05 14:57:34 +0100

    like/dislike

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TimelineComponent } from './timeline/timeline.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TimelineComponent} from './timeline/timeline.component';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TimelineComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TimelineComponent,
+        TimeAgoPipe
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -1,30 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { Tweet } from '../tweet';
+import {Component, OnInit} from '@angular/core';
+import {Tweet} from '../tweet';
+
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.less']
+    selector: 'app-timeline',
+    templateUrl: './timeline.component.html',
+    styleUrls: ['./timeline.component.less']
 })
 export class TimelineComponent implements OnInit {
-  tweets: Tweet[] = [
-     {
-      created_at: 'Wed Apr 05 12:30:12 +0000 2017',
-      id: 1,
-      text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
-      user: 'James Braddock'
-    },
-    {
-      created_at: 'Thu Apr 06 15:24:15 +0000 2017',
-      id: 2,
-      text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
-      user: 'Bob kelso'
-    },
-  ];
+    tweets: Tweet[] = [
+        {
+            created_at: 'Wed Apr 05 12:30:12 +0000 2017',
+            id: 1,
+            text: 'Je mets les pieds où je veux, Little John… et c\'est souvent dans la gueule.',
+            user: 'James Braddock',
+            favoriteCount: 0
+        },
+        {
+            created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+            id: 2,
+            text: 'Qui a deux pouces et qui s\'en fout ? Bob Kelso !',
+            user: 'Bob kelso',
+            favoriteCount: 0
+        },
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onSave(tweetText: HTMLInputElement) {
+
+        const newTweet = {};
+        newTweet.created_at = new Date();
+        newTweet.text = tweetText;
+        newTweet.user = 'quentin';
+        newTweet.id = this.tweets.length + 1;
+        this.tweets.push(newTweet);
+    }
+
+    onLike(tweet: object, like: HTMLInputElement, dislike: HTMLInputElement) {
+        tweet.favoriteCount++;
+        if (tweet.favoriteCount > 0) {
+            like.className = 'fas fa-arrow-alt-circle-up';
+            dislike.className = 'far fa-arrow-alt-circle-down';
+        }
+
+    }
+
+    onHover(like: HTMLInputElement) {
+        like.style.cursor = 'pointer';
+    }
+
+    onDislike(tweet: object, dislike: HTMLInputElement, like: HTMLInputElement) {
+        tweet.favoriteCount--;
+        if (tweet.favoriteCount < 0) {
+            dislike.className = 'fas fa-arrow-alt-circle-down';
+            like.className = 'far fa-arrow-alt-circle-up';
+        }
+
+
+    }
 
 }
